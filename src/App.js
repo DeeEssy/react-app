53529617a6c7e4b568e2827593f4d26943b39904
DeeEssy <venchesters.2002@gmail.com>
2020-10-31 22:05:12 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Header from './components/Header';
 import SideBar from './components/sideBar';
 import Profile from './components/Profile';
 
-
+const App = () => {
+  return (
+    <div className="page">
+      <Header />
+      <SideBar />
+      <Profile />
+    </div>
+  );
+};
 
 export default App;
